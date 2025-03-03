355af2a4b7417083e10334cab8b4e4550342942d
LYIVX <tommyleee1@googlemail.com>
2025-03-03 15:24:14 +0000

    fix: Update Next.js config for dynamic routes and image domains

--- next.config.js
+++ next.config.js
@@ -1,28 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "cdn.discordapp.com",
-      "mc-heads.net",
-      "cdn.discordapp.com/embed/avatars",
-    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.discordapp.com",
-        pathname: "/avatars/**",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.discordapp.com",
-        pathname: "/embed/avatars/**",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "mc-heads.net",
-        pathname: "/avatar/**",
+        pathname: "/**",
       },
     ],
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
