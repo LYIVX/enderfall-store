9eb6fc49e1e49de4e97f4cba767eba755be68ad4
LYIVX <tommyleee1@googlemail.com>
2025-03-03 15:22:12 +0000

    fix: Update Next.js image configuration for Discord avatars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.discordapp.com", "mc-heads.net"],
+    domains: [
+      "cdn.discordapp.com",
+      "mc-heads.net",
+      "cdn.discordapp.com/embed/avatars",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/avatars/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/embed/avatars/**",
+      },
+      {
+        protocol: "https",
+        hostname: "mc-heads.net",
+        pathname: "/avatar/**",
+      },
+    ],
   },
 };
 
